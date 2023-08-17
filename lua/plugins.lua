36cf2c5447a739cd38d0bb02db42704ec2562ae1
Dmitry Lakhno <dmitry@vsyaco.com>
2023-08-17 21:13:29 +0300

    Updates

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,16 +2,16 @@ local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.n
 
 -- Install packer from github if is not in our system
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = vim.fn.system({
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  })
-  print("Installing packer close and reopen Neovim...")
-  vim.cmd([[packadd packer.nvim]])
+    PACKER_BOOTSTRAP = vim.fn.system({
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    })
+    print("Installing packer close and reopen Neovim...")
+    vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -25,80 +25,81 @@ vim.cmd([[
 -- Use a protected require call (pcall) so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+    return
 end
 
 -- Show packer messages in a popup. Looks cooler
 packer.init({
-  display = {
-    open_fn = function()
-      return require("packer.util").float({ border = "rounded" })
-    end,
-  },
+    display = {
+        open_fn = function()
+            return require("packer.util").float({ border = "rounded" })
+        end,
+    },
 })
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use 'vim-airline/vim-airline'
-  use 'vim-airline/vim-airline-themes'
-  use 'tpope/vim-fugitive'
-  use 'airblade/vim-gitgutter'
-  use 'drewtempelmeyer/palenight.vim'
-  use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.2',
-      requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use {
-    'mg979/vim-visual-multi',
-    branch = 'master'
-  }
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function()
-      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-      ts_update()
-    end,
-    config = function()
-      require("config.nvim-treesitter")
-    end,
-  }
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    requires = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
-      {                                      -- Optional
-        'williamboman/mason.nvim',
-        run = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end,
-      },
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
-
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
+    use 'vim-airline/vim-airline'
+    use 'vim-airline/vim-airline-themes'
+    use 'tpope/vim-fugitive'
+    use 'airblade/vim-gitgutter'
+    use 'drewtempelmeyer/palenight.vim'
+    -- use 'folke/tokyonight.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
-  }
-  use("github/copilot.vim")
-  -- Instal nvim-tree and set Ctrl-1 to toggle it
-  use {
-    'nvim-tree/nvim-tree.lua',
-    config = function()
-      require("config.nvim-tree")
-    end,
+    use {
+        'mg979/vim-visual-multi',
+        branch = 'master'
+    }
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+        config = function()
+            require("config.nvim-treesitter")
+        end,
+    }
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                            -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
+    use("github/copilot.vim")
+    -- Instal nvim-tree and set Ctrl-1 to toggle it
+    use {
+        'nvim-tree/nvim-tree.lua',
+        config = function()
+            require("config.nvim-tree")
+        end,
     }
 end)
